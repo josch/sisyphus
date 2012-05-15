e36d419a37622e7009deb4af0580884d493d90e0
josch <j.schauer@email.de>
2012-05-16 00:57:13 +0200

    pretty print packlist.xml

--- run.sh
+++ run.sh
@@ -9,6 +9,10 @@ rm -f score_max
 
 python bruteforce2.py $1 | sort -r | xargs --max-procs=4 --max-args=1 python bruteforce3.py $1 $2 $3
 
+mv $2 $2.tmp
+xmllint --output $2 --format $2.tmp
+rm -f $2.tmp
+
 echo palletViewer -o $1 -p $2 -s $3
 
 echo python evaluate.py $1 $2 $3
